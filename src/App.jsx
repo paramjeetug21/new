6249ad2f113d1e25e74816972c15c6479e86eb40
paramjeetug21 <paramjeet.ug21@nsut.ac.in>
2025-11-13 12:00:08 +0530

    functio

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./pages/Login";
 import { Todo } from "./pages/Todo";
 import { Signup } from "./pages/Signup";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Todo />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-]);
-
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Todo />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
